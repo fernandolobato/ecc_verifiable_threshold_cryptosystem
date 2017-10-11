370d230066e606f3cfd1356957fc35e9a7fade6c
fernandolobato <lobato.meeser.fernando@hotmail.com>
2017-10-11 13:43:57 -0500

    thresholw cryptosystem complete

--- threshold_cryptosystem.py
+++ threshold_cryptosystem.py
@@ -1,7 +1,12 @@
+#! /usr/bin/env python
 #
+# Provide an implementation of a (t,n) threshold cryptosystem. 
 #
+# Implementation of cryptographic scheme from:
+# 
 #
-#
+# Written in 2017 by Fernanddo Lobato Meeser and placed in the public domain.
+
 import hashlib
 import math
 import random
@@ -11,8 +16,19 @@ from ecdsa.curves import SECP256k1
 from ecdsa.ellipticcurve import Point
 
 
-def secret_split():
-    pass
+def secret_split(secret, t, n):
+    """
+    """
+    coef = [secret] + [randrange(SECP256k1.order) for i in range(1, t)]
+
+    f = lambda x: sum([ coef[i] * pow(x, i) for i in range(t)]) % SECP256k1.order
+
+    secret_share = [ f(i) for i in range(1, n + 1)]
+
+    F = [ coef[j] * SECP256k1.generator for j in range(t) ]
+    
+    return (secret_share, F)    
+
 
 def verify_secret_share(secret_share, i, F, G=SECP256k1.generator):
     """
@@ -36,41 +52,90 @@ def reconstruct_key(sub_secret_share, t):
         
         for h in range(1, t + 1):
             if h != j:
-
-                mult *= (h / (h - j))
+                mult *= ( h / (h - j))
 
         recon_key += sub_secret_share[j - 1] * int(mult)
 
     return recon_key % SECP256k1.order
 
 
-def encrypt():
-    pass
+def encrypt(pub_key, message):
+    """
+    """ 
+    k = randrange(SECP256k1.order)
+    
+    P = SECP256k1.generator * k
+    H = k * pub_key
 
-master_private_key = randrange(SECP256k1.order)
-master_public_key = master_private_key * SECP256k1.generator
+    c = message * H.y()
 
-t = 10
-n = 100
+    return (P, c)
 
-coef = [master_private_key] + [randrange(SECP256k1.order) for i in range(1, t)]
+def decrypt(sec_key, signature):
+    """
+    """
+    (P, c) = signature
+    H = sec_key * P
 
-f = lambda x: sum([ coef[i] * pow(x, i) for i in range(t)]) % SECP256k1.order
+    message = c * pow(H.y(), -1)
 
-secret_share = [ f(i) for i in range(1, n + 1)]
+    return round(message)
 
-F = [ coef[j] * SECP256k1.generator for j in range(t) ]
+
+def string_to_int(msg):
+    """
+    """
+    return int(''.join([ str(ord(c)) for c in msg]))
+
+def int_to_string(num):
+    """
+    """
+    num = str(num)
+    return ''.join([ chr(int(num[i:i+2])) for i in range(0,len(num),2)])
+
+
+
+def main():
+
+
+    master_private_key = randrange(SECP256k1.order)
+    master_public_key = master_private_key * SECP256k1.generator
+
+    t = 10
+    n = 20 
+
+    secret_share, F = secret_split(master_private_key, t, n)
+
+    message = 'GERM'
+
+    msg = string_to_int(message)
+    print(msg)
+
+    ciphertext = encrypt(master_public_key, msg)
+    msg_2 = decrypt(master_private_key, ciphertext)  
+
+    print(msg_2)
 
 # for i in range(n):
 #     assert(verify_secret_share(secret_share[i],i,F))
 
-r = secret_share[:]
-random.shuffle(r)
-
-r_key = reconstruct_key(secret_share[:t], t)
-
-print(r_key == master_private_key)
 
 
+# for i in range(10):
+#     msg = random.randint(1, 1000)
+#     ciphertext = encrypt(master_public_key, msg)
+#     msg_2 = decrypt(master_private_key, ciphertext)
+#     assert(msg_2 == msg)
+
+# print(string_to_int('abc'))
+
+
+# r_key = reconstruct_key(secret_share, t)
+
+# print(r_key == master_private_key)
+
+
+if __name__ == '__main__':
+    main()
 
 
